6129d0c93b2623a9e227594156dcafeae084c259
ejmeitz <54505069+ejmeitz@users.noreply.github.com>
2025-06-08 22:00:22 -0400

    add tests file

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using TDEP
+using Test
+
+data_dir = joinpath(@__DIR__, "data")
+
+
+#TODO Test run all commands on dummy data to ensure they are wrapped properly
+#TODO Try with only manditory args as well as ALL kwargs
