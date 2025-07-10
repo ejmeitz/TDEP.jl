42659985837d2d6f4d1dd84e46b0d18305ed8f6b
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-07-10 18:37:20 -0400

    working on TI

--- ext/MolecularDynamicsExt/TI.jl
+++ ext/MolecularDynamicsExt/TI.jl
@@ -1,128 +1,163 @@
-# export TI, HarmonicPotential
+export TI, HarmonicPotential
 
-# function TI(
-#         sys::System{3},
-#         sim::NVT,
-#         lambdas::AbstractVector;
-#         weights = ones(length(lambdas)) ./ length(lambdas)
-#     )
+function TI(
+        sys::System{3},
+        sim::NVT,
+        ifc2,
+        lambdas::AbstractVector;
+        weights = ones(length(lambdas)) ./ length(lambdas)
+    )
 
-#     f = (x) -> TI_core(sys, sim, x)
-#     return dot(weights, f.(lambdas))
+    f = (x) -> TI_core(sys, sim, x, ifc2)
+    return dot(weights, f.(lambdas))
 
+end
+
+
+function TI(
+        sys::System{3},
+        sim::NVT,
+        ifc2,
+        n_lambda::Integer,
+        quadrature_rule::Function = FastGaussQuadrature.gausslegendre
+    )
+
+    λ, w = quadrature_rule(n_lambda)
+    return TI(sys, sim, λ, ifc2; weights = w)
+
+end
+
+# Implement Molly.jl "General Interaction"
+#* TODO JUST CALL libolle.jl --> load IFC obj from path, call pot_eng
+struct HarmonicPotential{T, R}
+    ifc2::Matrix{T}
+    r₀::R
+    u_storage::R
+end
+
+function HarmonicPotential(ifc2, r₀)
+    T = eltype(ifc2)
+    return HarmonicPotential{T}(ifc2, r₀, similar(r₀))
+end
+
+# function HarmonicPotential(ifc2_path::String)
+#  #TODO
 # end
 
+function AtomsCalculators.forces(sys, inter::HarmonicPotential; kwargs...)
+    inter.u_storage .= sys.coords .- inter.r₀
+    one_T = one(float_type(sys)) # get 1.0 with proper floating point precision
+    return -one_T .* (inter.ifc2 * u_storage)
+end
 
-# function TI(
-#         sys::System{3},
-#         sim::NVT,
-#         n_lambda::Integer,
-#         quadrature_rule::Function = FastGaussQuadrature.gausslegendre
-#     )
-
-#     λ, w = quadrature_rule(n_lambda)
-#     return TI(sys, sim, λ; weights = w)
-
-# end
-
-# # Implement Molly.jl "General Interaction"
-# #* TODO JUST CALL libolle.jl --> load IFC obj from path, call pot_eng
-# struct HarmonicPotential{T, R}
-#     ifc2::Matrix{T}
-#     r₀::R
-#     u_storage::R
-# end
-
-# function HarmonicPotential(ifc2, r₀)
-#     T = eltype(ifc2)
-#     return HarmonicPotential{T}(ifc2, r₀, similar(r₀))
-# end
-
-# # function HarmonicPotential(ifc2_path::String)
-# #  #TODO
-# # end
-
-# function AtomsCalculators.forces(sys, inter::HarmonicPotential; kwargs...)
-#     inter.u_storage .= sys.coords .- inter.r₀
-#     one_T = one(float_type(sys)) # get 1.0 with proper floating point precision
-#     return -one_T .* (inter.ifc2 * u_storage)
-# end
-
-# function AtomsCalculators.potential_energy(sys, inter::HarmonicPotential; kwargs...)
-#     FT = float_type(sys)
-#     half =  FT(0.5)
-#     return half * ((transpose(inter.u_storage) * ifc2) * inter.u_storage)
-# end
+function AtomsCalculators.potential_energy(sys, inter::HarmonicPotential; kwargs...)
+    FT = float_type(sys)
+    half =  FT(0.5)
+    return half * ((transpose(inter.u_storage) * ifc2) * inter.u_storage)
+end
 
 
-# struct MixedHamiltonian{A, B, C <: HarmonicPotential, U}
-#     λ::A
-#     H1::B #* FOR NOW ASSUME ITS A SINGLE PAIR POTENTIAL
-#     H0::C
-#     ΔU_storage::U
-# end
+struct MixedHamiltonian{A, B, C <: HarmonicPotential, U}
+    λ::A
+    H1::B #* FOR NOW ASSUME ITS A SINGLE PAIR POTENTIAL
+    H0::C
+    ΔU_storage::U
+end
 
-# function AtomsCalculators.forces(sys, inter::MixedHamiltonian; kwargs...)
+function MixedHamiltonian(λ, H1, H0, n_steps; energy_units = u"eV")
+    ΔU_storage = zeros(n_steps) * energy_units
+    return MixedHamiltonian{typeof(λ), typeof(H1), typeof(H0), typeof(ΔU_storage)}(λ, H1, H0, ΔU_storage)
+end
 
-#     F1 = ...
-#     F0 = AtomsCalculators.forces(sys, inter.H0; kwargs...)
+function AtomsCalculators.forces(sys, inter::MixedHamiltonian; 
+                                    n_threads=Threads.nthreads(), kwargs...)
 
-#     return ((1-inter.λ) * F0) + inter.λ*F1
-# end
+    F0 = AtomsCalculators.forces(sys, inter.H0; kwargs...)
 
-# function AtomsCalculators.potential_energy(sys, inter::MixedHamiltonian; step_n = 0, kwargs...)
+    #* ASSUMES PAIR POTENTIAL
+    neighbors = find_neighbors(sys; n_threads=n_threads)
+    pairwise_inters_nonl = filter(!use_neighbors, values(sys.pairwise_inters))
+    pairwise_inters_nl   = filter( use_neighbors, values(sys.pairwise_inters))
+    if n_threads > 1
+        pairwise_forces_threads!(fs_nounits, fs_chunks, sys.atoms, sys.coords, sys.velocities,
+                                    sys.boundary, neighbors, sys.force_units, length(sys),
+                                    pairwise_inters_nonl, pairwise_inters_nl, n_threads, step_n)
+    else
+        pairwise_forces!(fs_nounits, sys.atoms, sys.coords, sys.velocities, sys.boundary,
+                            neighbors, sys.force_units, length(sys), pairwise_inters_nonl,
+                            pairwise_inters_nl, step_n)
+    end
+
+    return ((1-inter.λ) * F0) + inter.λ*F1
+end
+
+function AtomsCalculators.potential_energy(sys, inter::MixedHamiltonian; 
+                                            step_n = 0, n_threads=Threads.nthreads(), kwargs...)
     
-#     U1 = ...
-#     U0 = AtomsCalculators.potential_energy(sys, inter.H0; kwargs...)
+    U0 = AtomsCalculators.potential_energy(sys, inter.H0; kwargs...)
 
-#     inter.ΔU_storage[step_n] = U1 - U0
+    #* ASSUMES PAIR POTENTIAL
+    neighbors = find_neighbors(sys; n_threads=n_threads)
+    pairwise_inters_nonl = filter(!use_neighbors, values(sys.pairwise_inters))
+    pairwise_inters_nl   = filter( use_neighbors, values(sys.pairwise_inters))
+    if n_threads > 1
+        U1 = pairwise_pe_threads(sys.atoms, sys.coords, sys.velocities, sys.boundary,
+                                    neighbors, sys.energy_units, length(sys), pairwise_inters_nonl,
+                                    pairwise_inters_nl, T, n_threads, step_n)
+    else
+        U1 = pairwise_pe(sys.atoms, sys.coords, sys.velocities, sys.boundary, neighbors,
+                            sys.energy_units, length(sys), pairwise_inters_nonl,
+                            pairwise_inters_nl, T, step_n)
+    end
 
-#     return ((1-inter.λ) * U0) + inter.λ*U1
+    inter.ΔU_storage[step_n] = U1 - U0
 
-# end
+    return ((1-inter.λ) * U0) + inter.λ*U1
+end
 
+# Runs MD simulation to approximate ΔU
+function TI_core(sys::System{3}, sim::NVT, λ, ifc2)
 
-# #* HAVE USER DEFINE "REAL" POTENTIAL IN sys OBJECT, I ADD THE HARMONIC POTENTIAL IN 
-# #* BUT YOU CANT APPLY WEIGHTS THSI WAY
+    if length(sys.loggers) > 0
+        @warn "Found $(length(sys.loggers)) loggers, removing from system"
+    end
 
-# # Runs MD simulation to approximate ΔU
-# function TI_core(sys::System{3}, sim::NVT, λ)
+    length_units = u"Å"
+    energy_units =  u"eV"
 
-#     if length(sys.loggers) > 0
-#         @warn "Found $(length(sys.loggers)) loggers, removing from system"
-#     end
+    LT = typeof(1.0*length_units)
+    ET = typeof(1.0*energy_units)
 
-#     length_units = u"Å"
-#     energy_units =  u"eV"
+    new_loggers = (
+        # coords = CoordinatesLogger(LT, sim.sample_every), 
+        pe = PotentialEnergyLogger(ET, sim.sample_every), 
+    )
 
-#     LT = typeof(1.0*length_units)
-#     ET = typeof(1.0*energy_units)
+    #* HOW TO LOAD IFCS
+    hp = HarmonicPotential(ifc2, copy(sys.coords))
 
-#     new_loggers = (
-#         coords = CoordinatesLogger(LT, sim.sample_every), 
-#         pe = PotentialEnergyLogger(ET, sim.sample_every), 
-#     )
+    pot = LennardJones(cutoff=ShiftedForceCutoff(8.5u"Å"), )
 
-#     #* HOW TO LOAD IFCS
-#     hp = HarmonicPotential(ifc2, copy(sys.coords))
-
-#     mixed_H = MixedHamiltonian(
-#         λ, 
-#         #* HOW,
-#         hp
-#     )
-
-
-#     new_system = System(
-#         deepcopy(sys);
-#         loggers = deepcopy(new_loggers),
-#         force_units = energy_units / length_units,
-#         energy_units = energy_units,
-#         k = Molly.default_k(energy_units)
-#     )
-
-#     run_sim!(new_system, sim)
+    mixed_H = MixedHamiltonian(
+        λ, 
+        pot,
+        hp,
+        sim.n_steps;
+        energy_units = energy_units
+    )
 
 
+    new_system = System(
+        deepcopy(sys);
+        loggers = deepcopy(new_loggers),
+        general_inters = (mixed_H,),
+        force_units = energy_units / length_units,
+        energy_units = energy_units,
+        k = Molly.default_k(energy_units)
+    )
 
-# end
+    run_sim!(new_system, sim)
+
+    return mixed_H.ΔU_storage
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+
+using SimpleCrystals
 using Molly
 using TDEP
 using Test
@@ -43,25 +45,38 @@ end
 
     using ASEconvert
     using PythonCall
+    ase_lj = pyimport("ase.calculators.lj")
+
 
     basedir = "/mnt/merged/emeitz/tdep_jl_test/cc_test"
-    
-    a = 5.2468u"Å" # Lattice parameter for FCC Argon at 10 K
-    fcc_crystal = SimpleCrystals.FCC(a, :Ar, SVector(4, 4, 4))
 
-    ase_calc = pyimport("ase.calculators.lj").LennardJones(sigma = 3.4, epsilon = 0.010423, rc = 8.5)
+    # Settings
+    a = 5.2468u"Å"
+    r_cut = 8.5
+    temperature = 10.0 #K
+    max_freq = 2.5 #THz
+    nconf = 20 # number of configurations per iteration
+    niter = 2 # number of self-consistent iterations
+
+    # Create Structure
+    fcc_crystal = SimpleCrystals.FCC(a, :Ar, SVector(4, 4, 4))
+    to_ucposcar(fcc_crystal, joinpath(basedir, "infile.ucposcar"))
+
+    # Create Force Calculator
+    ase_calc = ase_lj.LennardJones(sigma = 3.4, epsilon = 0.010423, rc = r_cut)
     calc = ASEcalculator(ase_calc)
 
+    # Run sTDEP
     sTDEP(
-        fcc_crystal,
-        calc,
-        basedir,
-        2,
-        20,
-        8.5,
-        10.0,
-        2.5
-    )
+            fcc_crystal,
+            calc,
+            basedir,
+            niter,
+            nconf,
+            r_cut,
+            temperature,
+            max_freq
+        )
 
 end
 
@@ -70,24 +85,54 @@ end
     outdir = "/mnt/merged/emeitz/tdep_jl_test"
 
     # Settings
-    a = 5.2468u"Å" # Lattice parameter for FCC Argon at 10 K
+    a = 5.2468u"Å" 
+    r_cut = 8.5u"Å"
     temp = 10.0u"K"
     damping = 1.0u"ps^-1"
     dt = 1.0u"fs"
     n_steps_warmup = 2000
     n_steps = 25_000
     sample_every = 1_000
+    outdir = "/mnt/merged/emeitz/tdep_jl_test/group_meeting"
 
-    # Create Structure
     fcc_crystal = SimpleCrystals.FCC(a, :Ar, SVector(4, 4, 4))
-    to_ucposcar(fcc_crystal, joinpath(rundir, "infile.ucposcar"))
+    to_ucposcar(fcc_crystal, joinpath(outdir, "infile.ucposcar"))
 
-    sim = NVT(temperature, damping, dt, n_steps_warmup, n_steps, sample_every)
 
-    generate_MDTDEP_dataset(fcc_crystal, sim, outdir; n_seeds = 2)
+    # Configure MD (Molly.jl)
+    # Details of this are unimportant, read their docs
 
-    efc = ExtractForceConstants(secondorder_cutoff = ustrip(r_cut))
-    execute(efc, rundir)
+    pot = LennardJones(cutoff=ShiftedForceCutoff(r_cut), )
+
+    sys = System(
+        fcc_crystal;
+        pairwise_inters=(pot,),
+        energy_units=u"eV",
+        force_units=u"eV * Å^-1",
+    )
+
+    # Set sigma and epsilon parameters properly
+    σ = 3.4u"Å"
+    ϵ = 0.010423u"eV"
+    updated_atoms = []
+
+    for i in eachindex(sys)
+        push!(updated_atoms, Molly.Atom(index=sys.atoms[i].index, atom_type=sys.atoms[i].atom_type,
+                                mass=sys.atoms[i].mass, charge=sys.atoms[i].charge,
+                                σ=σ, ϵ=ϵ))
+    end
+
+    sys = System(sys; atoms=[updated_atoms...])
+
+
+    # Generate MD Dataset (TDEP.jl)
+    sim = NVT(temp, damping, dt, n_steps_warmup, n_steps, sample_every)
+    generate_MDTDEP_dataset(sys, sim, outdir; n_seeds = 2)
+
+    # Extract IFCs (TDEP.jl)
+    ncores = 10
+    efc = ExtractForceConstants(secondorder_cutoff = 4.0)
+    execute(efc, outdir, ncores)
 
 end
 
@@ -135,4 +180,58 @@ end
     sim = NVT(temperature, damping, dt, n_steps_warmup, n_steps, sample_every)
     generate_MDTDEP_dataset(sys, sim, outdir; n_seeds = 2)
 
+end
+
+@testset "TI Pair Potential" begin
+
+    ifc_path = ""
+    n_uc = 4
+    a = 5.2468u"Å" # Lattice parameter for FCC Argon at 10 K
+    temp = 10.0u"K"
+    damping = 1.0u"ps^-1"
+    dt = 1.0u"fs"
+    n_steps_warmup = 2000
+    n_steps = 25_000
+    sample_every = 1_000
+    n_lambda = 13
+
+    fcc_crystal = SimpleCrystals.FCC(a, :Ar, SVector(n_uc, n_uc, n_uc))
+    m = mass(fcc_crystal, 1)
+
+    energy_unit = u"eV"
+    length_unit = u"Å"
+    ifc2_unit = energy_unit / length_unit^2
+
+    ifc2 = (jldopen(ifc_path, "dynmat") .* ustrip(m)) * ifc2_unit
+
+    pot = LennardJones(cutoff=ShiftedForceCutoff(r_cut), )
+
+    sys = System(
+        fcc_crystal;
+        pairwise_inters=(pot,),
+        energy_units=u"eV",
+        force_units=u"eV * Å^-1",
+    )
+
+    # Set sigma and epsilon parameters properly
+    σ = 3.4u"Å"
+    ϵ = 0.010423u"eV"
+    updated_atoms = []
+
+    for i in eachindex(sys)
+        push!(updated_atoms, Molly.Atom(index=sys.atoms[i].index, atom_type=sys.atoms[i].atom_type,
+                                mass=sys.atoms[i].mass, charge=sys.atoms[i].charge,
+                                σ=σ, ϵ=ϵ))
+    end
+
+    sys = System(sys; atoms=[updated_atoms...])
+
+    sim = NVT(temperature, damping, dt, n_steps_warmup, n_steps, sample_every)
+
+    TI(
+        sys,
+        sim,
+        ifc2,
+        n_lambda,
+    )
 end
